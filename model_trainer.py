a91b5743cb63424048c0dfbefbeac2bb51938697
USAFA <usafa@usafa-ai-nx-r1-3.edu.usafa.edu>
2023-02-22 10:28:52 -0700

    added temporary model, pipeline is almost complete, need inegration from model to roverX

--- model_trainer.py
+++ model_trainer.py
@@ -2,6 +2,27 @@ import pyrealsense2.pyrealsense2 as rs
 from cv2 import resize, inRange, imshow, waitKey
 from os import chdir
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+height = 160
+width = 320
+
+def build_model():
+    headings = layers.Input(shape=(1,), name='headings')
+    images = layers.Input(shape=(height, width,1), name='images')
+    conv1 = layers.Conv2D(32, (3, 3), activation='relu')(images)
+    pool1 = layers.MaxPooling2D((2, 2))(conv1)
+    flatten = layers.Flatten()(pool1)
+    dense1 = layers.Dense(32, activation='relu')(flatten)
+    concat = layers.Concatenate()([dense1, headings])
+    dense2 = layers.Dense(16, activation='relu')(concat)
+    outputs = layers.Dense(2, name='outputs')(dense2)
+
+    model = keras.Model(inputs=[headings, images], outputs=outputs)
+
+    return model.compile(optimizer='adam', loss={'outputs': 'mse'},
+                  metrics={'throttle': 'mae', 'steering': 'mae'})
 
 
 def get_attributes(csv_string):
@@ -16,7 +37,7 @@ def process_img(img):
     img_bw = inRange(rs_img, white_threshold, white)
     return  img_bw[0:320][80:240]
 
-def process_train_file(file):
+def process_train_file(file,model):
     bag_file = file + ".bag"
     csv_file =  "csvs/" + file + ".csv"
 
@@ -52,7 +73,13 @@ def process_train_file(file):
             img = np.asanyarray(frame.get_data())
             frames.append(process_img(img))
 
-            
+
+
+        trained = model.fit({'headings':  inputs, 'image': frames},{'outputs':outputs}, epochs = 1, verbose = 1)
+
+
+
+
             # imshow("img", process_img(img))
             # waitKey(1)
 
@@ -62,7 +89,9 @@ def process_train_file(file):
 
         #Write code to train model 100 items at a time
 chdir("/media/usafa/extern_data/Team Just Kidding/Collections/")
-process_train_file("c_1.1_0313_1040_42")
+
+model = build_model()
+process_train_file("c_1.1_0313_1040_42" ,model)
 
 
 
