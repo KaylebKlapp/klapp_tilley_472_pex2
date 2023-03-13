aa22d169b702e23e2c25a707605e7713f53d9e10
Kayleb Klapp <C24Kayleb.Klapp@afacademy.af.edu>
2023-03-13 14:03:58 -0600

    Added condition for ensuring the drive is connected

---
+++ bash_copy.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -d "/media/usafa/extern_data/Team Just Kidding/Collections" ] 
+then
+    cp "/media/usafa/PycharmProjects/USAFA/pex2/*.bag" "/media/usafa/extern_data/Team Just Kidding/Collections"
+    cp "/media/usafa/PycharmProjects/USAFA/pex2/csvs/*" "/media/usafa/extern_data/Team Just Kidding/Collections/csvs/"
+    rm "/media/usafa/PycharmProjects/USAFA/pex2/*.bag"
+    rm "/media/usafa/PycharmProjects/USAFA/pex2/csvs/*"
+    echo "File copy complete"
+else
+    echo "Path not found."
