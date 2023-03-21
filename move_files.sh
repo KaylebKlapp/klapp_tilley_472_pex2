813bd6a22cd02729ecb6152bf2ba8249da437685
Kayleb Klapp <C24Kayleb.Klapp@afacademy.af.edu>
2023-03-21 08:34:57 -0600

    Bash script update

--- move_files.sh
+++ move_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-if [ -d "/media/usafa/extern_data/Team Just Kidding/Collections" ] 
-then
+if [ -d "/media/usafa/extern_data/Team Just Kidding/Collections" ]; then
     cp "/media/usafa/PycharmProjects/USAFA/pex2/*.bag" "/media/usafa/extern_data/Team Just Kidding/Collections"
     cp "/media/usafa/PycharmProjects/USAFA/pex2/csvs/*" "/media/usafa/extern_data/Team Just Kidding/Collections/csvs/"
     rm "/media/usafa/PycharmProjects/USAFA/pex2/*.bag"
