0df3fd3d049a2cdf1033da217c571b8280b4db31
Kayleb Klapp <C24Kayleb.Klapp@afacademy.af.edu>
2023-03-21 08:39:06 -0600

    Bash script update

--- move_files.sh
+++ move_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -d "/media/usafa/extern_data/Team Just Kidding/Collections" ]; then
-    cp "/media/usafa/PycharmProjects/USAFA/pex2/*.bag" "/media/usafa/extern_data/Team Just Kidding/Collections"
-    cp "/media/usafa/PycharmProjects/USAFA/pex2/csvs/*" "/media/usafa/extern_data/Team Just Kidding/Collections/csvs/"
-    rm "/media/usafa/PycharmProjects/USAFA/pex2/*.bag"
-    rm "/media/usafa/PycharmProjects/USAFA/pex2/csvs/*"
+    cp "/media/usafa/data/telemetry/*.bag" "/media/usafa/extern_data/Team Just Kidding/Collections"
+    cp "/media/usafa/data/telemetry/csvs/*" "/media/usafa/extern_data/Team Just Kidding/Collections/csvs/"
+    rm "/media/usafa/data/telemetry/*.bag"
+    rm "/media/usafa/data/telemetry/csvs/*"
     echo "File copy complete"
 else
     echo "Path not found."
