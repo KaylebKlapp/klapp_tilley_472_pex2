48c1fce324b119e40f5b3721b017e41bcdfab52e
Kayleb Klapp <C24Kayleb.Klapp@afacademy.af.edu>
2023-03-22 11:55:55 -0600

    autorun

--- auto_run.py
+++ auto_run.py
@@ -44,8 +44,8 @@ def run():
         preds =  get_predictions(heading, p_frame)
         steering  = int(preds[0][0])
         throttle = int(preds[0][1])
-        steering = 0 if steering < 0 else steering
-        throttle = 0 if throttle < 0 else throttle
+        steering = 1500 #0 if steering < 0 else steering
+        throttle = 1600 #0 if throttle < 0 else throttle
 
 
         drone.channels.overrides = {'1': steering, '3': throttle}
