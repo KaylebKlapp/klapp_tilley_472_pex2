af65aeef2411ff5efb62b4da0fc4109d1ab13ef7
KaylebKlapp <90409926+KaylebKlapp@users.noreply.github.com>
2023-03-22 18:19:32 -0600

    Add files via upload

--- model_trainer.py
+++ model_trainer.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from glob import glob
+import matplotlib.pyplot as plt
 from random import sample
 height = 160
 width = 320
@@ -14,28 +15,27 @@ width = 320
 def build_model():
     headings = layers.Input(shape=(1,), name='headings')
     images = layers.Input(shape=(height, width,1), name='images')
-    conv1 = layers.Conv2D(64, (6, 6), activation='relu')(images)
+    conv1 = layers.Conv2D(32, (6, 6), activation='relu')(images)
     conv2 = layers.Conv2D(32, (3, 3), activation='relu')(conv1)
-    #pool1 = layers.MaxPooling2D((2, 2))(conv1)
-    flatten = layers.Flatten()(conv2)
+    pool1 = layers.MaxPooling2D((2, 2))(conv2)
+    flatten = layers.Flatten()(pool1)
     dense1 = layers.Dense(32, activation='relu')(flatten)
-    #dense11 = layers.Dense(8, activation='relu')(dense1)
     concat = layers.Concatenate()([dense1, headings])
     dense2 = layers.Dense(64, activation='relu')(concat)
-    dense3 = layers.Dense(32, activation='relu')(dense2)
-    dense4 = layers.Dense(32, activation='relu')(dense3)
-    outputs = layers.Dense(2, name='outputs')(dense4)
+    dense4 = layers.Dense(32, activation='relu')(dense2)
+    dense5 = layers.Dense(16, activation='relu')(dense4)
+    outputs = layers.Dense(2, name='outputs')(dense5)
 
     model = keras.Model(inputs=[headings, images], outputs=outputs)
-    model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='mse', metrics=[keras.losses.mean_absolute_percentage_error])
     model.summary()
     return model
 
 
 def get_attributes(csv_string):
     csv_string = csv_string.rstrip()
-    heading, steering, throttle, index = [int(val) for val in csv_string.split(",")]
-    return heading, steering, throttle, index
+    throttle, steering, heading, index = [int(val) for val in csv_string.split(",")]
+    return throttle, steering, heading, index
 
 
 white_threshold = np.array([215, 215, 215])
@@ -69,7 +69,7 @@ def generate_training_data(batch_size = 512):
 
             generic_file_name = file_name.rstrip(".bag")
             bag_file = file_name
-            csv_file =  "csvs/" + generic_file_name + ".csv"
+            csv_file = "csvs/" + generic_file_name + ".csv"
 
             pipeline = create_pipeline(bag_file)
 
@@ -99,7 +99,7 @@ def generate_training_data(batch_size = 512):
                     if line == "":
                         break
 
-                    heading, steering, throttle, index = get_attributes(line)
+                    throttle, steering, heading, index = get_attributes(line)
                     indexes.append(index)
 
                     frame = pipeline.wait_for_frames().get_color_frame()
@@ -120,34 +120,34 @@ def generate_training_data(batch_size = 512):
             #rename(csv_file, f"TRAINED_{csv_file}")
             #rename(csv_file, f"TRAINED_{bag_file}")
 
-def generate_validation_data(file_path, num_samples=32):
-    bag_file = file_path
-    csv_file = bag_file.rstrip(".bag")
-    csv_file = "csvs/" + csv_file + ".csv"
-    csv_fp = open(csv_file, "r")
-    lines = csv_fp.readlines()
-    num_lines = len(lines)
-    offset = int(num_lines / num_samples)
-
-    validation_frames = [] 
+def generate_validation_data(num_samples = 1024):
+    num_samples = num_samples/2
+    validation_frames = []
     validation_inputs = []
-    validation_outputs = [] 
+    validation_outputs = []
+    for file in glob("*VALIDATION*", recursive= False):
+        bag_file = file
+        csv_file = bag_file.rstrip(".bag")
+        csv_file = "csvs/" + csv_file + ".csv"
+        csv_fp = open(csv_file, "r")
+        lines = csv_fp.readlines()
+        num_lines = len(lines)
+        offset = int(num_lines / num_samples)
 
-    pipeline = create_pipeline(bag_file)
-    old_heading = 0
-    for i in range(0, num_lines, offset):
-        old_heading, _, _ , _= get_attributes(lines[i - 1]) if i != 1 else [0,0,0,0]
-        heading, steering, throttle, index = get_attributes(lines[i])
-        frame = pipeline.wait_for_frames().get_color_frame()
-
-        while frame.frame_number < index:
+        pipeline = create_pipeline(bag_file)
+        for i in range(0, num_lines, offset):
+            _, _, old_heading, _ = get_attributes(lines[i - 1]) if i != 1 else [0,0,0,0]
+            throttle, steering, heading, index = get_attributes(lines[i])
             frame = pipeline.wait_for_frames().get_color_frame()
 
-        validation_inputs.append(heading - old_heading)
-        validation_frames.append(process_img(np.asanyarray(frame.get_data())))
-        validation_outputs.append(np.array([steering, throttle]))
+            while frame.frame_number < index:
+                frame = pipeline.wait_for_frames().get_color_frame()
 
-    return [np.array(validation_inputs),np.array(validation_frames)], np.array(validation_outputs)
+            validation_inputs.append(heading - old_heading)
+            validation_frames.append(process_img(np.asanyarray(frame.get_data())))
+            validation_outputs.append(np.array([steering, throttle]))
+
+    return [np.array(validation_inputs), np.array(validation_frames)], np.array(validation_outputs)
 
 
 def get_samples_length():
@@ -166,24 +166,33 @@ def get_samples_length():
 model_version = 2
 def train_model(model):
     batch_size = 32
-    epochs = 1000
+    epochs = 20
     x_dat = generate_training_data(batch_size=batch_size)
     step_num = get_samples_length() / batch_size
-    # DEVICE = "GPU:1"
-    # with tf.device(DEVICE):
-    save_fp = f'{curdir}/Models/model_{model_version}_'+'{epoch:04d}_{val_loss:.3f}.hdf5'
-    save_best = keras.callbacks.ModelCheckpoint(filepath=save_fp, monitor='val_loss', save_best_only=True)
-    history = model.fit(
-        x=x_dat,
-        validation_data=(validation_data, validation_y),
-        batch_size=batch_size,
-        steps_per_epoch=step_num,
-        epochs=epochs,
-        verbose=1,
-        callbacks=[save_best])
+    DEVICE = '/GPU:0'
+    with tf.device(DEVICE):
+        model = build_model()
+        save_fp = f'{curdir}/Models/model_{model_version}_'+'{epoch:04d}_{val_loss:.3f}.hdf5'
+        save_best = keras.callbacks.ModelCheckpoint(filepath=save_fp, monitor='val_loss', save_best_only=True)
+        history = model.fit(
+            x=x_dat,
+            validation_data=(validation_data, validation_y),
+            batch_size=batch_size,
+            steps_per_epoch=step_num,
+            epochs=epochs,
+            verbose=1,
+            callbacks=[save_best])
     chdir(curdir)
     print(f"Saving model to {curdir}")
     model.save("trained_model")
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['train', 'validation'])
+    plt.show()
 
 
 curdir = getcwd()
@@ -198,8 +207,9 @@ else:
     chdir("C:/Users/C23Jason.Tilley/Desktop/AI/Collections")
 
 
-validation_data, validation_y = generate_validation_data("VALIDATION.bag")
-model = build_model()
+validation_data, validation_y = generate_validation_data()
+print(len(validation_data[0]))
+model = None
 train_model(model)
 
 
